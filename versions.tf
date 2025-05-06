1d1f1247a3d5a4b5f6e528af3eed6c174a8d2f36
avmugesh <46862942+avmugesh@users.noreply.github.com>
2025-05-06 13:15:30 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.6.0"
 
   backend "s3" {
-    bucket         = "terraform-state-myproject"
+    bucket         = "terraform-state-myproject1"
     key            = "state/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
